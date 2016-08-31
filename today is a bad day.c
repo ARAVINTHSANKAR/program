0e059c69348b52be0e5d57803939d3dd2c647b1a
ARAVINTHSANKAR <aravinthsankareee@gmail.com>
2016-08-31 19:19:06 +0530

    Add files via upload

---
+++ today is a bad day.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+ char a[170]="today is a bad day";
+ char b[170];
+ int i;
+for(i=0;i<=170;i++)
+{
+    if(a[i]=='a')
+    {
+      a[i]='e';
+    }
+    }
+    for(i=0;i<=170;i++)
+    {
+    b[i]=a[i];
+    }
+b[0]=(toupper(b[0]));
+b[6]=(toupper(b[6]));
+b[9]=(toupper(b[9]));
+b[11]=(toupper(b[11]));
+b[15]=(toupper(b[15]));
+
+for(i=0;i<170;i++)
+{
+printf("%c",b[i]);
+ }
+}
